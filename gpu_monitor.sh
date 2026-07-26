ed4904ded194f86f83befe3fdd660124dabf1f2e
Richo Yudha Kurniawan <richoyudha027@gmail.com>
2026-07-26 12:55:22 +0700

    update gitignore

--- gpu_monitor.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-OUT="gpu_monitor_$(date +%Y%m%d_%H%M%S).csv"
-nvidia-smi --query-gpu=timestamp,index,utilization.gpu,utilization.memory,clocks.sm,temperature.gpu,power.draw,memory.used \
-  --format=csv,nounits -l 5 >> "$OUT"
